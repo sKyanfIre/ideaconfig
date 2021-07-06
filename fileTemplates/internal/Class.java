a1922823c4647d02355cf1ef34b7db1faea4a296
zhangtianyu <zhangtianyu@yupei-scm.com>
2021-07-06 15:34:28 +0800

    IU-2021.1.2 <600900@C25 Create baseRefactoring.xml, markdown.xml, vim_settings.xml, p3c.xml, ide.general.xml, _@user_One Dark Vivid Italic.icls, ide-features-trainer.xml, _@user_Atom One Dark _Material_.icls, project.default.xml, lombok-plugin.xml, yiiguxing.translation.xml, shared-indexes.xml, Default.xml, visualizationTool.xml, file.template.settings.xml, find.xml, one_dark_config.xml, ui.lnf.xml, filetypes.xml, keymap.xml, androidEditors.xml, memory.view.xml, colors.scheme.xml, _@user_Material Oceanic.icls, java.java, databaseDrivers.xml, templates.xml, Windows copy.xml, GrepConsole.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, acceptedLanguageLevels.xml, intentionSettings.xml, wsl.distributions.xml, material_theme.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, Default2.xml, editor.xml, _@user_Monokai Pro _Material_.icls, debugger.xml, MyGroup.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @ClassName ${NAME}
+* @description TODO
+* @author zhangtianyu
+* @date ${DATE} ${TIME}
+* @version 1.0
+**/
+public class ${NAME} {
+}
